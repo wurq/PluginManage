e88bb6f562aa6b8260a8b67db52c8615c47a446f
wurongqiu <wurongqiu@gmail.com>
2017-02-21 15:27:06 +0800

    init mainactivity

--- app/src/main/java/com/wurq/dex/pluginmanage/MainActivity.java
+++ app/src/main/java/com/wurq/dex/pluginmanage/MainActivity.java
@@ -1,13 +1,140 @@
 package com.wurq.dex.pluginmanage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.wurq.dex.pluginlibrary.Util;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<PluginItem> mPluginItems = new ArrayList<PluginItem>();
+    private PluginAdapter mPluginAdapter;
+
+    private ListView mListView;
+    private TextView mNoPluginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
     }
+
+    private void initView() {
+        mPluginAdapter = new PluginAdapter();
+        mListView = (ListView) findViewById(R.id.plugin_list);
+        mNoPluginTextView = (TextView)findViewById(R.id.noplugin);
+    }
+
+    private void initData() {
+        String pluginFolder = Environment.getExternalStorageDirectory() + "/host";
+        File file = new File(pluginFolder);
+        File[] plugins = file.listFiles();
+        if (plugins == null || plugins.length == 0) {
+            mNoPluginTextView.setVisibility(View.VISIBLE);
+            return;
+        }
+
+        for (File plugin : plugins) {
+            PluginItem item = new PluginItem();
+            item.pluginPath = plugin.getAbsolutePath();
+            item.packageInfo = Util.getPackageInfo(this, item.pluginPath);
+            if (item.packageInfo.activities != null && item.packageInfo.activities.length > 0)
+            {
+                item.launcherActivityName = item.packageInfo.activities[0].name;
+            }
+
+            mPluginItems.add(item);
+        }
+
+        mListView.setAdapter(mPluginAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                PluginItem item = mPluginItems.get(position);
+
+            }
+        });
+        mPluginAdapter.notifyDataSetChanged();
+    }
+
+
+    private class PluginAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        public PluginAdapter() {
+            mInflater = MainActivity.this.getLayoutInflater();
+        }
+
+        @Override
+        public int getCount() {
+            return mPluginItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mPluginItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.plugin_item, parent, false);
+                holder = new ViewHolder();
+                holder.appIcon = (ImageView) convertView.findViewById(R.id.app_icon);
+                holder.appName = (TextView) convertView.findViewById(R.id.app_name);
+                holder.apkName = (TextView) convertView.findViewById(R.id.apk_name);
+                holder.packageName = (TextView) convertView.findViewById(R.id.package_name);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            PluginItem item = mPluginItems.get(position);
+            PackageInfo packageInfo = item.packageInfo;
+//            holder.appIcon.setImageDrawable(DLUtils.getAppIcon(MainActivity.this, item.pluginPath));
+//            holder.appName.setText(DLUtils.getAppLabel(MainActivity.this, item.pluginPath));
+            holder.apkName.setText(item.pluginPath.substring(item.pluginPath.lastIndexOf(File.separatorChar) + 1));
+            holder.packageName.setText(packageInfo.applicationInfo.packageName + "\n" +
+                    item.launcherActivityName + "\n" );
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+        public ImageView appIcon;
+        public TextView appName;
+        public TextView apkName;
+        public TextView packageName;
+    }
+
+    public static class PluginItem {
+        public PackageInfo packageInfo;
+        public String pluginPath;
+        public String launcherActivityName;
+
+        public PluginItem() {
+        }
+    }
+
 }
